fd5b7f51936402dd0ab9f2e4f018347874d8713e
Franco Carlo Bertinetti <101714220+Fran1239@users.noreply.github.com>
2023-09-28 17:11:21 -0300

    v.2

--- whatsappservice.py
+++ whatsappservice.py
@@ -3,7 +3,7 @@ import json
 
 def SendMessageWhatsapp(data):
     try:
-        token = "EAAUQM1QonnkBOy8MCE6hNart3Ix0OMlftN9ZBjyoKTjUZCmWXxzMqMs4PpE68m1AkpVVA4g9m6PcSzPZAzQmajujLMec7qCVNA8nMhc5beGd4QcJJD3vd3PoSKXscWWI4q7qpFkkeJs9q6BzM60xgZAshxsk4IiWqbVMD476lHrO5CL2z6cHjny3SdtaPDk0lYvO"
+        token = "EAAUQM1QonnkBOwQiPxCcZBf2v2Te60aQolgRMnrAGq1mYWlmbL2nAolXhkHsYTOWCGoZB5nMZB4pNJjy9g0UqLXZCT3nKhaZACdNaZARXZCf2j7Pa69ARx0kKucYa1u0JxEb3P2lnI1ayK9hSkh4vpuZBsUlsHxbg9CpNXXZAPRZBG9rC0YjyDIiPuA5giWc2ZBhhXveCpvpZCaZBm5DvjIp48NnKBHCujx4ogAdeaOLrDd8ZD"
         api_url = "https://graph.facebook.com/v17.0/114784988391901/messages"
         headers = {"Content-Type": "application/json", "Authorization": "Bearer " + token}
         response = requests.post(api_url, data = json.dumps(data), headers = headers)
